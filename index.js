abdf9fb24b5900339558939a89e3f040ea278afe
Mbianou Bradon <mbianoubradon2000@gmail.com>
2022-12-12 17:30:23 +0100

    Intro to React

---
+++ index.js
@@ -0,0 +1,15 @@
+function MainContain(){
+    return (
+        <h1>I am currently learning React</h1>
+    )
+}
+
+
+
+ReactDOM.render(
+    <div>
+        <MainContain />
+    </div>,
+    
+    document.getElementById('root')
+)
