8631a8d392182d4bedf1ebead0b9d4f63c592cbf
Mbianou Bradon <mbianoubradon2000@gmail.com>
2022-12-12 18:27:48 +0100

    Custom Component in reactJS

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function MainContain(){
-    return (
-        <h1>I am currently learning React</h1>
+function ReactReason() {
+    return(
+        <header>
+            <h2>Why I am learning ReactJS</h2>
+            <ul>
+                <li>It makes life easier</li>
+                <li>It is a requirement for a front-end developer</li>
+                <li>I kinda don't have choice</li>
+            </ul>
+        </header>
     )
 }
 
-
-
-ReactDOM.render(
-    <div>
-        <MainContain />
-    </div>,
-    
+ReactDOM.render (
+    <ReactReason />,
     document.getElementById('root')
 )
