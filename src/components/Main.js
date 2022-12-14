0f25760b3ac071b867640c3d55bb55135f2da3a8
Mbianou Bradon <mbianoubradon2000@gmail.com>
2022-12-14 16:12:00 +0100

    setting up react environment

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function Main(){
+    return (
+        <div>
+            <Navbar />
+            <h1>Main content goes here</h1>
+        </div>
+    )
+}
